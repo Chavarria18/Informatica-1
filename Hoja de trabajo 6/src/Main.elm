13393ba16b65840e1597e43774623d204efc680c
Chavarria18 <chavarria181386@unis.edu.gt>
2018-09-13 15:43:00 -0700

    corrección archivos

--- Hoja de trabajo 6/src/Main.elm
+++ Hoja de trabajo 6/src/Main.elm
@@ -43,3 +43,29 @@ contn (n,y) =
     else if esPrimo y == False
     then contn (n, y + 1)
     else y:: contn (n - 1, y + 1) --repite el proceso
+
+--reglas de procedencia
+
+type Natural = Suc Natural | Cero  
+
+
+
+enteroAnatural : Int -> Natural
+enteroAnatural i = if i == 0 then Cero else Suc (enteroAnatural(i-1))
+
+sumar n1 n2 = case (n1, n2) of 
+    (Cero, n2_) -> n2_
+    (n1_, Cero) -> n1_
+    (Suc n1_, Suc n2_) ->  Suc (Suc (sumar n1_ n2_))
+
+restar n1 n2 = case (n1, n2) of 
+    (Cero, n2_) -> Cero
+    (n1_, Cero) -> n1_
+    (Suc n1_, Suc n2_) ->  restar n1_ n2_
+
+
+
+mult n1 n2 = case (n1,n2) of
+    (Cero, n2_) -> Cero
+    (n1_,Cero) -> Cero
+    (Suc n1_, n2_) -> 
