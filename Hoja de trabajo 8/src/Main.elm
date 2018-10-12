5e6b35413c50d97b431515b7b57aaea8dd4fdeb6
Chavarria18 <chavarria181386@unis.edu.gt>
2018-10-11 17:18:41 -0700

    Subeindo hoja 8

--- Hoja de trabajo 8/src/Main.elm
+++ Hoja de trabajo 8/src/Main.elm
@@ -8,27 +8,32 @@ zipWith aux lista1 lista2 = case (lista1, lista2) of
     (lista1_, []) -> []
     (x::xs, y::ys) -> aux x y :: zipWith aux xs ys
 
+    --> zipWith (+) [4,2,5,6] [2,6,2,3] 
+
 --Ejercicio 2
 
 groupBy : (a -> Bool) -> List a -> (List a, List a) 
-groupBy z list = (funciona z list, nofunciona z list)
+groupBy z list = (acepta z list, noAcepta z list)
 
-funciona a list = case list of 
+acepta a list = case list of 
     [] -> []
     (x::xss) -> if a x 
-                then funciona a xss 
-                else x:: funciona a xss
+                then acepta a xss 
+                else x:: acepta a xss
 
-nofunciona a list = case list of 
+noAcepta a list = case list of 
     [] -> []
     (b::bss) -> if a b 
-                then b:: nofunciona a bss 
-                else nofunciona a bss
-
+                then b:: noAcepta a bss 
+                else noAcepta a bss
+  
+  
+  -- groupBy ((<)4) [1,2,3,4,5,6,7,8,9]
 
 --Ejercicio 3
+
 bind : Maybe a -> (a -> Maybe b) -> Maybe b
 bind xs b = 
   case xs of 
     Nothing -> Nothing 
-    Just a -> b a
+    Just a -> b a  
