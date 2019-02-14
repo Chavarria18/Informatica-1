a07c39312e83bdb4cd5fe346b49bd981d887d4a9
Gabriel <chavarria181386@unis.edu.gt>
2019-02-14 12:11:54 -0600

    subiendo lab3

--- Semsetre3/Laboratorio3/lab.3.cc
+++ Semsetre3/Laboratorio3/lab.3.cc
@@ -5,85 +5,123 @@ using namespace std;
 int main(){
 
 char p;
+char salida;
 float g = 0.0;
 float k = 0.0;
-float t = 0;
+float t = 0;//tiempo contador
 float t2 = 0;
 int vo = 0; //velocidad original
-float vtotal = 0;
-float dtotal = 0;
 
 
-cout<<"Ingrese un cáracter:\n";
+printf("Escoja un caracter de la sigiente lista:\n Tierra = t\n Venus = v\n Luna = l\n Callisto = c\n");
+cout<<"Ingrese el caracter deseado:\n";
 std::cin >> p;
 
 switch(p){
     case 't':
     g = 9.8;
     k = 0.0023;
-    
     printf("La gravedad es: %.2f, la resistencia del viento es: %.4f\n", g ,k);
-   
+    printf("Ingrese el tiempo de caida libre:\n");
+    cin >> t2;//solicitud del tiempo
+    while(t2 >= t ){
+        float fatm = k * (pow(vo,2));
+        float f = g - fatm;
+        float a = f ;
+        float v = v + (a * (0.01));
+        float d = d + (v * (0.01));
+        t += 0.01;
+        vo = v;
+      
+        
+        if(t2 <= t){
+            printf("La distancia es %.2f \nLa velocidad es %.2f \n",d ,v);
+            }
+        }
+    break; 
     
-    printf("Ingrese el tiempo de caida libre: ");
+    case 'v':
+    g = 8.87;
+    k = 0.0023;
+    printf("La gravedad es: %.2f, la resistencia del viento es: %.4f\n", g ,k);
+    printf("Ingrese el tiempo de caida libre:\n");
     cin >> t2;
     while(t2 >= t ){
         float fatm = k * (pow(vo,2));
         float f = g - fatm;
         float a = f ;
-        float v = a * t;
-        float d = v * t;
+        float v = v + (a * (0.01));
+        float d = d + (v * (0.01));
         t += 0.01;
         vo = v;
+      
+        
         if(t2 <= t){
-        printf("La distancia es %.2f \nLa velocidad es %.2f \n",d ,v);}
-        
-        
-        
+            printf("La distancia es %.2f \nLa velocidad es %.2f \n",d ,v);
+            }
         }
-    
-
-    
-    
-   
-    
-    break; 
-
-
-
-
-
-
-
-
-
-
-    case 'v':
-    g = 8.87;
-    k = 0.0023;
-    
     break;
     
     case 'l':
     g = 1.62;
     k = 0.0;
-
-    
+    printf("La gravedad es: %.2f, la resistencia del viento es: %.4f\n", g ,k);
+    printf("Ingrese el tiempo de caida libre:\n");
+    cin >> t2;
+    while(t2 >= t ){
+        float fatm = k * (pow(vo,2));
+        float f = g - fatm;
+        float a = f ;
+        float v = v + (a * (0.01));
+        float d = d + (v * (0.01));
+        t += 0.01;
+        vo = v;
+      
+        
+        if(t2 <= t){
+            printf("La distancia es %.2f \nLa velocidad es %.2f \n",d ,v);
+            }
+        }
     break;
     
     case 'c':
     g = 1.23;
     k = 0.0;
-   
+    printf("La gravedad es: %.2f, la resistencia del viento es: %.4f\n", g ,k);
+    printf("Ingrese el tiempo de caida libre:\n");
+    cin >> t2;
+    while(t2 >= t ){
+        float fatm = k * (pow(vo,2));
+        float f = g - fatm;
+        float a = f ;
+        float v = v + (a * (0.01));
+        float d = d + (v * (0.01));
+        t += 0.01;
+        vo = v;
+      
+        
+        if(t2 <= t){
+            printf("La distancia es %.2f \nLa velocidad es %.2f \n",d ,v);
+            }
+        }
     break;
 
-    default: cout << "Entrada invalida,"; main(); 
-
-    }
-
-
-
-return 0; 
-
+default: cout << "Entrada invalida,"; main(); 
 }
-    
+cout<<"Desea salir del programa (s/n)?:\n";cin >> salida;
+switch(salida){
+        case 's':
+        system("clear");
+        return 0;
+        break;
+
+        case 'n':
+        system("clear");
+        main();
+        break;
+        }
+return 0; 
+}
+
+
+//Se aproximo la respuesta a dos decimales. 
