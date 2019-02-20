705fe7c02be2a666f1efefcb0f00dd717b67f659
Gabriel <chavarria181386@unis.edu.gt>
2019-02-20 16:26:14 -0600

    subiendo parte 2

--- Semsetre3/Laboratorio4/4.cc
+++ Semsetre3/Laboratorio4/4.cc
@@ -1,41 +1,45 @@
 #include <iostream>
-#include<math.h>
+
 using namespace std;
 //Parte 2 
 
 
-void vP(const float x[], const float y[], int dims){
-   float r[]={}; 
+float productoPunto(const float x[], const float y[], int dims){
+   float total = 0; 
+   float suma = 0; 
+   float r= 0; //funciona?
     for(int z = 0; z < dims; z++){
-        r[z]= (x[z]*y[z]); 
-    //return 0.0; 
+        r= (x[z]*y[z]); 
+        total = total + r; 
+   
+    }
+    printf("El producto punto es: %.2f",total);
+    return total; 
 }
-}
+
+
 
 int main(){
    printf("Ingrese la dimension deseada:\n ");
    int dims;
    cin >> dims;
- 
    
-   float x[dims];
-   float y[dims]; 
-   float r[dims];
+   float x1[dims];
+   float y1[dims]; 
+   //float r[dims];
 
-    printf("Ingrese el primer vecotor:\n ");
+    printf("Ingrese el primer vector:\n ");
     for(int z=0; z < dims; z++){
-        cin>> x[z];
+        cin>> x1[z];
     }
-    printf("Ingrese el segundo  vecotor:\n  ");
-    for(int z=0; z < dims; z++){
-        cin>> x[z];
+    printf("Ingrese el segundo  vector:\n  ");
+    for(int j=0; j < dims; j++){
+        cin>> y1[j];
     }
-    int suma = 0; 
-
-    vP(x, y, dims);
-    printf("El resultado es:\n  ");
-
+    productoPunto(x1, y1,dims);
+      
  
     
 
 }
+
