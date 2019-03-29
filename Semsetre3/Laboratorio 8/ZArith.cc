4464a3af3de21b22a46961a1f6ae56758fcd5a23
Gabriel <chavarria181386@unis.edu.gt>
2019-03-28 20:12:51 -0600

    subiendo lab 8

--- Semsetre3/Laboratorio 8/ZArith.cc
+++ Semsetre3/Laboratorio 8/ZArith.cc
@@ -2,13 +2,58 @@
 
 ZArith::ZArith(int _base): _base(_base){}
 
+bool parseInt(const std::string& valor, int& resultado){
+    
+    int tam = valor.size();
+    int i = 0;
+    for(int i = 0; i < tam; i++)
+    {
+        if(valor[i] < '0' || valor[i] > '9' ){
+            
+            if (i == ( tam - 1 )){
+                resultado = std::stoi( valor ); 
+            return true;
+            }
+        }else{
+            return false;
+        }
+        
+    }
+    return false;
+} 
+
 
 bool ZArith::parse(const std::string valor, int& resultado)const{
-   
+      int respuesta = 0;
+    int tam = valor.size(); 
+    std::string parte1 , parte2;
 
+    if (parseInt(valor, resultado)){
+        for(int i = 0; i < tam; i++){
+            
+            if(valor[i] == '+' || valor[i] > '*' ){
+                parte1= valor[i-1];
+                parte1 = arg1;
+                parte2 = valor[i+1];
+                parte2 = arg2;
+                if(valor[i] == '+'){
+                    respuesta = ZArith::opSuma(arg1,arg2);
+                    
+                }else if(valor[i] == '+'){
 
+                    respuesta = ZArith::opProducto(arg1,arg2);
+                }
+            }else 
+            {            
+            return false; 
+        }
+        }
+            return true; 
 
-}//este es el que falta 
+    }
+    return true; 
+
+}
 
 
 int ZArith::opSuma(const int arg1, const int arg2)const{
@@ -27,4 +72,7 @@ int ZArith::opProducto(const int arg1, const int arg2)const{
 
 }
 
-int main(){}
+
+
+
+//int main(){}
